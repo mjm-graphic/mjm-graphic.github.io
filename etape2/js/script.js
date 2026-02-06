16281802ad268ce16de7a57fa3113362abdb4307
mjm-graphic <matteo.bleuvialon@mjm-etudiants.com>
2026-02-06 16:56:10 +0100

    Add files via upload

---
+++ etape2/js/script.js
@@ -0,0 +1,25 @@
+function verifradio() {
+  const reponse = document.querySelector('input[name="reponse"]:checked');
+
+  const indice = document.getElementById("indice");
+  const error = document.getElementById("error");
+  const gif = document.getElementById("gif");
+
+  if (!reponse) {
+    alert("Choisis une réponse 😉");
+    return;
+  }
+
+  // ⬇️ Définis ici la bonne réponse
+  if (reponse.value === "a") {
+    indice.style.display = "block";
+    error.style.display = "none";
+    gif.style.display = "block";
+  } else {
+
+    error.style.display = "block";
+    indice.style.display = "none";
+    gif.style.display = "none";
+
+  }
+}
