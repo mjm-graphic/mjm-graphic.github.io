c638a6d8941987c7894d0d64c3c00b1353931f44
mjm-graphic <matteo.bleuvialon@mjm-etudiants.com>
2026-02-06 10:34:19 +0100

    Update script.js

--- MDP/js/script.js
+++ MDP/js/script.js
@@ -1,7 +1,7 @@
 function verifmdp() {
   let mdp = document.getElementById("password").value;
+  mdp.toLowerCase()
   if (mdp === "oui") {
-    mdp.toLowerCase()
     document.getElementById("indice").style.display = "block";
     document.getElementById("error").style.display = "none";
     document.getElementById("icon-lock").style.display = "none";
